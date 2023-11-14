ddab6cecdf9e315626c8b1a2616bca3f2d4516ec
LanaVol <svolosh4yk@gmail.com>
2023-11-15 00:34:39 +0200

    create start project files & start mockup BEM

---
+++ src/data/data.js
@@ -0,0 +1,13 @@
+const forecasts = [
+  {
+    category: "Футбол",
+    date: "Завтра",
+    time: "9:20",
+    team1: "Металлург Магнитогорск",
+    labelTeam1: "",
+    team2: "Трактор",
+    labelTeam2: "",
+    dateForecast: "11.04.2022",
+    popular: true,
+  },
+];
