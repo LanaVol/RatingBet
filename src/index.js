0bbbd4224fd360098e89f4235fa97ab32635ca2e
LanaVol <svolosh4yk@gmail.com>
2023-11-16 01:07:45 +0200

    update

--- src/index.js
+++ src/index.js
@@ -1,26 +1,26 @@
-import "./styles/global.scss";
 import "./styles/styles.scss";
+import "./styles/global.scss";
 
-class Forecast {
-  constructor(
-    category,
-    date,
-    time,
-    team1,
-    labelTeam1,
-    team2,
-    labelTeam2,
-    dateForecast,
-    popular
-  ) {
-    this.category = category;
-    this.date = date;
-    this.time = time;
-    this.team1 = team1;
-    this.labelTeam1 = labelTeam1;
-    this.team2 = team2;
-    this.labelTeam2 = labelTeam2;
-    this.dateForecast = dateForecast;
-    this.popular = popular;
-  }
-}
+// class Forecast {
+//   constructor(
+//     category,
+//     date,
+//     time,
+//     team1,
+//     labelTeam1,
+//     team2,
+//     labelTeam2,
+//     dateForecast,
+//     popular
+//   ) {
+//     this.category = category;
+//     this.date = date;
+//     this.time = time;
+//     this.team1 = team1;
+//     this.labelTeam1 = labelTeam1;
+//     this.team2 = team2;
+//     this.labelTeam2 = labelTeam2;
+//     this.dateForecast = dateForecast;
+//     this.popular = popular;
+//   }
+// }
