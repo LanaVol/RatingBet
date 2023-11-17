7eda345a3e81ad274fec5d132776a6c43605af5a
LanaVol <svolosh4yk@gmail.com>
2023-11-17 23:01:56 +0200

    create fs for header-menu mobile

--- src/index.js
+++ src/index.js
@@ -24,3 +24,20 @@ import "./styles/global.scss";
 //     this.popular = popular;
 //   }
 // }
+
+const body = document.querySelector("body");
+const menu = document.querySelector(".menu");
+const bg = document.querySelector(".bground");
+body.addEventListener("click", handleClick);
+
+function handleClick(e) {
+  if (e.target.classList.contains("header__burger")) {
+    bg.classList.add("bg_active");
+    menu.classList.add("header__menu_mobile-active");
+  }
+
+  if (e.target.classList.contains("close-btn") && menu.classList.contains("header__menu_mobile-active")) {
+    bg.classList.remove("bg_active");
+    menu.classList.remove("header__menu_mobile-active");
+  }
+}
