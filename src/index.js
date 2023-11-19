1edeb196c221f58789c7d934bd23442e67478113
LanaVol <svolosh4yk@gmail.com>
2023-11-19 22:02:46 +0200

    updated according utils fs

--- src/index.js
+++ src/index.js
@@ -3,44 +3,5 @@ import "./styles/global.scss";
 import "./widgets/section3/leagues.js";
 import "./widgets/section1/forecastsCards.js";
 import "./widgets/section2/matchCenter.js";
-
-// class Forecast {
-//   constructor(
-//     category,
-//     date,
-//     time,
-//     team1,
-//     labelTeam1,
-//     team2,
-//     labelTeam2,
-//     dateForecast,
-//     popular
-//   ) {
-//     this.category = category;
-//     this.date = date;
-//     this.time = time;
-//     this.team1 = team1;
-//     this.labelTeam1 = labelTeam1;
-//     this.team2 = team2;
-//     this.labelTeam2 = labelTeam2;
-//     this.dateForecast = dateForecast;
-//     this.popular = popular;
-//   }
-// }
-
-const body = document.querySelector("body");
-const menu = document.querySelector(".menu");
-const bg = document.querySelector(".bground");
-body.addEventListener("click", handleClick);
-
-function handleClick(e) {
-  if (e.target.classList.contains("header__burger")) {
-    bg.classList.add("bg_active");
-    menu.classList.add("header__menu_mobile-active");
-  }
-
-  if (e.target.classList.contains("close-btn") && menu.classList.contains("header__menu_mobile-active")) {
-    bg.classList.remove("bg_active");
-    menu.classList.remove("header__menu_mobile-active");
-  }
-}
+import "./widgets/utils/menuFunctions.js";
+import "./widgets/utils/scrollingUtils.js";
